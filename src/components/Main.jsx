8cced920bb7441884f2bea04d4a960b3566c88b6
bowen-anth <bowen.anth@gmail.com>
2023-12-17 09:22:48 -0700

    added utils folder with functions.jsx file and added cursor pointer on hover for nav links and pay and transfer buttons

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,18 +1,36 @@
-import React from "react"
+import React, { useState } from "react"
 import accounts from "./data/accountsData.jsx"
 
 export default function Main() {
+
+    const [selected, setSelected] = useState({
+        mainAccount: false,
+        expenses: false,
+        savings: false
+    })
+console.log(selected)
     return (
         <>
             <main>
                 <div className="button-container">
                     <button>Pay</button> <button>Transfer</button>
                 </div>
-                <div className="accounts">
-
-                </div>
-                <div clasName="spendings">
-
+                <div className="accounts-container">
+                    {!selected.mainAccount && 
+                    <div className="accounts">
+                        <h2>Accounts</h2>
+                    </div>
+                    }
+                    {selected.mainAccount &&
+                    <div className="accounts-spendings-container"> 
+                        <div className="accounts">
+                            <h2>Accounts</h2>
+                        </div>
+                        <div className="spendings">
+                            <h2>Spendings</h2>
+                        </div>
+                    </div>
+                    }
                 </div>
             </main>
         </>
