191dda17be4a00773b79d12b34892d1ff617ba65
bowen-anth <bowen.anth@gmail.com>
2023-12-16 20:10:50 -0700

    added components

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import accounts from "./data/accountsData.jsx"
+
+export default function Main() {
+    return (
+        <>
+            <header>
+                <nav>
+                    {console.log(accounts)}
+                </nav>
+            </header>
+        </>
+    )
+}
---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+export default function Nav() {
+    return (
+        <>
+            <header>
+                <nav>
+                    <h1>I am H1</h1>
+                </nav>
+            </header>
+        </>
+    )
+}
